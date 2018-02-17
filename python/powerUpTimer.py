438c5e7c3707d879e6e2ae5cab1e0f988a2a3767
David A. Rush <david@rushtone.com>
2018-02-17 10:13:02 -0500

    Working 1 sec samples, with context-specific names now (rather than generic names the example came with).

--- python/powerUpTimer.py
+++ python/powerUpTimer.py
@@ -22,6 +22,9 @@ BAUD_RATE = 9600
 
 REMOTE_NODE_ID = "SCALE"
 
+LEFT = IOLine.DIO0_AD0
+RIGHT = IOLine.DIO1_AD1
+
 DIGITAL_LINE = IOLine.DIO3_AD3
 ANALOG_LINE = IOLine.DIO2_AD2
 
@@ -32,35 +35,47 @@ def main():
     print("Power Up Timer!  Looking for coordinator XBee on port " + PORT)
 
     localDevice = XBeeDevice(PORT, BAUD_RATE)
+    print("localDevice = %s", localDevice)
 
     try:
         localDevice.open()
+        print("Opened localDevice = ", localDevice)
 
         # Obtain the remote XBee localDevice from the XBee network.
         xbee_network = localDevice.get_network()
+        print("Found network: ", xbee_network)
+        print("Looking for remoteDevice named ", REMOTE_NODE_ID)
         remoteDevice = xbee_network.discover_device(REMOTE_NODE_ID)
         if remoteDevice is None:
-            print("Could not find the remote device")
+            print("Could not find the remote device named " + REMOTE_NODE_ID)
             exit(1)
+        print("Found remoteDevice = ", remoteDevice)
 
         # Set the local localDevice as destination address of the remote.
         remoteDevice.set_dest_address(localDevice.get_64bit_addr())
 
-        remoteDevice.set_io_configuration(DIGITAL_LINE, IOMode.DIGITAL_IN)
-        remoteDevice.set_io_configuration(ANALOG_LINE, IOMode.ADC)
+        remoteDevice.set_io_configuration(LEFT, IOMode.DIGITAL_IN)
+        remoteDevice.set_io_configuration(RIGHT, IOMode.DIGITAL_IN)
+        #remoteDevice.set_io_configuration(DIGITAL_LINE, IOMode.DIGITAL_IN)
+        #remoteDevice.set_io_configuration(ANALOG_LINE, IOMode.ADC)
 
         # Enable periodic sampling every IO_SAMPLING_RATE seconds in the remote device.
         remoteDevice.set_io_sampling_rate(IO_SAMPLING_RATE)
 
         # Enable DIO change detection in the remote device.
-        remoteDevice.set_dio_change_detection({DIGITAL_LINE})
+        remoteDevice.set_dio_change_detection({LEFT})
+        remoteDevice.set_dio_change_detection({RIGHT})
 
         # Register a listener to handle the samples received by the local device.
         def io_samples_callback(sample, remote, time):
-            print("New sample received from %s - %s" % (remote.get_64bit_addr(), sample))
+            print("got sample")
+            print("New sample from %s - %s" % (remote.get_node_id(), sample))
 
+        print("Registering callback on ", remoteDevice)
         localDevice.add_io_sample_received_callback(io_samples_callback)
 
+        # Wait for an input line to exit
+        print("Press enter to exit")
         input()
 
     finally:
